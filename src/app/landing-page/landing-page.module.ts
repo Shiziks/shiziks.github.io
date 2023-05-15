1a98a0cf04e0dd92ca2506b724416fa1ac405cb6
Em <shiziks@hotmail.com>
2023-05-15 14:43:19 +0200

    intro section html and css added

--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -5,12 +5,14 @@ import { LandingPageRoutingModule } from './landing-page-routing.module';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { SharedModule } from '../shared/shared.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IntroSectionComponent } from './components/intro-section/intro-section.component';
 
 
 
 @NgModule({
   declarations: [
-    LandingPageComponent
+    LandingPageComponent,
+    IntroSectionComponent
   ],
   imports: [
     CommonModule,
